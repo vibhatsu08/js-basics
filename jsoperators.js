2766fe27251a0f66b480d2d64568e06c08d42d28
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-15 02:57:17 +0530

    JS operators.
    This file contains the operators usually used in JS.
    including the ternary operators and 'typeof' operator.

---
+++ jsoperators.js
@@ -0,0 +1,62 @@
+/*var a=10;
+var b=12;
+
+//arithmetic operators
+//addition
+console.log(a+b);
+//subtraction
+console.log(a-b);
+//multiplication
+console.log(a*b);
+//division
+console.log(a/b);
+//modulus
+console.log(a%b);
+//increment operator
+console.log("value of a is " + a);
+console.log("value of a is now " + ++a);
+//decrement operator
+console.log("value of b is " + b);
+console.log("value of b is now " + --b);
+
+
+var c=14;
+var d=16;
+var result;
+
+//comparison operators
+//equal to '=='
+result=(c==d);
+console.log(result);
+//less than or equal to '<='
+result=(c<=d);
+console.log(result);
+//greater than or equal to '>='
+result=(c>=d);
+console.log(result);
+//not equal to
+result=(c!=d);
+console.log(result);
+
+
+//ternary operator
+var a=10;
+var b=12;
+var result;
+
+result=(a>b)?a:b;
+console.log(result);
+
+result=(a<b)?a:b;
+console.log(result);*/
+
+//more examples of ternary operators using the 'typeof' operator
+var a=12;
+var b="hello boy";
+var result;
+
+result=(typeof b=="string")?"b is a string":"b is a number";
+console.log(result);
+
+result=(typeof a=="number")?"a is a number":"a is a string";
+console.log(result);
