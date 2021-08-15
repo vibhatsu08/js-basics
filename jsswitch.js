8f65bbe0fbb7b2d43536c439bc9fc4d78ef9fe19
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-15 19:00:38 +0530

    switch case in JS.

---
+++ jsswitch.js
@@ -0,0 +1,20 @@
+//program to find the grade for the student using switch statement.
+var grade='a';
+
+switch (grade) {
+    case 'a':
+        console.log("grade a, excellent");
+        break;
+
+    case 'b':
+        console.log("grade b, good");
+        break;
+
+    case 'c':
+        console.log("grade c, not good");
+        break;
+
+    default:
+        console.log("need to improve");
+        break;
+}
