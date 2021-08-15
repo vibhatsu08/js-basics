2fb70644c855e22dbfba288e0f33403b89b52c83
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-15 18:56:21 +0530

    if else programs in javascript.
    this contains, if, if else, and if else if statements of javascript.

---
+++ jsifelse.js
@@ -0,0 +1,17 @@
+//variables declaration
+var subject="math";
+if(subject=="history"){
+    console.log("history book");
+}
+else if(subject=="math"){
+    console.log("math book");
+}
+else if(subject=="science"){
+    console.log("science book");
+}
+else if(subject=="cs"){
+    console.log("cs book");
+}
+else{
+    console.log("not valid");
+}
