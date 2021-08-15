31c669eb8cc83e463ecd72b07f6be924028d5c91
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-15 22:21:21 +0530

    while and do while loops in js.

---
+++ jswhile.js
@@ -0,0 +1,13 @@
+/*//while loops in javascript/
+var count=0;
+
+while(count<10){
+    console.log(count++);
+}*/
+
+//do while loop
+var count=0;
+
+do{
+    console.log(count++);
+}while(count<=10);
