3a1b560e180675b84255aedaf78ff2f5dd18cb46
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-15 22:31:01 +0530

    loop control in js.
    this has the break statement, continue statement, and the one with internal labels to control the flow.

---
+++ jsloopcontrol.js
@@ -0,0 +1,23 @@
+/*//loop control
+
+//break statement
+var count;
+
+for(count=0; count<=10; count++){
+    console.log(count);
+    if(count==5){
+        console.log("number " + count + " reached.");
+        break;
+    }
+}
+
+//continue statement
+var count;
+
+for(count=0; count<=10; count++){
+    if(count==3){
+        continue;
+    }
+    console.log(count);
+}*/
+
