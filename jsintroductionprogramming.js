0d9cf6fd1bf91845bb202efcac01581912c10aed
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-19 18:03:10 +0530

    JS intro examples.
    All the programming example for introduction into javascript programming.

---
+++ jsintroductionprogramming.js
@@ -0,0 +1,42 @@
+/*//js program to print hello world.
+console.log("hello, world!");
+
+//js program to add two numbers
+var num1 = 10;
+var num2 = 20;
+console.log(num1 + num2);
+
+//js program to find the square root of a number.
+var num = 80;
+console.log(Math.sqrt(num));
+
+//js program to find the area of a triangle
+var base = 20;
+var height = 30;
+var area = 0.5 * base * height;
+console.log(area);
+
+//js program to swap two variables.
+var num1 = 10;
+var num2 = 20;
+
+num1 = num1 + num2; //30
+num2 = num1 - num2; //30-20
+num1 = num1 - num2;
+
+console.log("num1: " + num1);
+console.log("num2: " + num2);
+
+//js program to convert km to miles.
+var km = 1;
+var miles = km / 1.6;
+
+console.log(miles);
+
+//js program to generate a random number.
+var random = Math.random();
+console.log(random);*/
+
+//js program to check if a number is positive negative or zero.
+var num;
+
